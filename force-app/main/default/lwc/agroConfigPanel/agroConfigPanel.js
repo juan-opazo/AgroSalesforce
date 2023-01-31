915dc987787e7f9909731864df34fdf66f2e4ffc
Juan Opazo <dopazo178@gmail.com>
2023-01-31 09:50:23 -0500

    apex, lwc, and metadata

--- force-app/main/default/lwc/agroConfigPanel/agroConfigPanel.js
+++ force-app/main/default/lwc/agroConfigPanel/agroConfigPanel.js
@@ -1,3 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getSAEmailParams from '@salesforce/apex/SoilAnalysisController.getSAEmailParams';
+import updateSAEmailParams from '@salesforce/apex/SoilAnalysisController.updateSAEmailParams';
 
-export default class AgroConfigPanel extends LightningElement {}
+export default class AgroConfigPanel extends LightningElement {
+    saEmailParams;
+
+    @wire(getSAEmailParams)
+    wiredCustomMetadata({ err, data }) {
+        if (data) {
+            this.saEmailParams = data;
+            let checkboxSASendEmail = this.template.querySelector('[data-id="checkbox-SA-send-email"]');
+            checkboxSASendEmail.checked = this.saEmailParams.isActive;
+            console.log(data);
+        } else {
+            console.error(err);
+        }
+    } 
+    handleActivateEmailSending(event) {
+        let checkboxSASendEmail = this.template.querySelector('[data-id="checkbox-SA-send-email"]');
+        checkboxSASendEmail.checked = event.target.checked;
+    }
+    updateSAEmailParams(event) {
+        updateSAEmailParams(this.saEmailParams);
+    }
+}
